2a4854d2d05a7c92e2b8b7969f9117fd0df38f83
Luis Gonzalez <162064187+LuisJG8@users.noreply.github.com>
2024-09-08 15:41:35 -0400

    Add files via upload

--- flaskWeb.py
+++ flaskWeb.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template
 from flask_menu import Menu, register_menu
+import requests
+
+
+
+
 
 
 app = Flask(__name__)
@@ -7,6 +12,23 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("index.html")
+
+
+@app.route('/<name>')
+def user(name):
+    connection = requests.get(f"https://api.genderize.io?name={name}").json()
+    print(connection)
+    name = connection["name"]
+    gender = connection["gender"]
+    print(name, gender)
+
+    connection_two = requests.get(f"https://api.agify.io?name={name}").json()
+    print(connection_two)
+    name_two = connection_two["name"]
+    age = connection_two["age"]
+    print(name_two, age)
+
+    return render_template("index.html", name=name, gender=gender, age=age)
 
 
 # @app.route("/user/<path:name>/")
